6c9204816cac5a9c7533710cde38690f5f511de1
Mlodovico <murilo.lodovico@outlook.com>
2020-06-16 13:16:30 -0300

    1.10.55 useAAuth hook

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -7,7 +7,7 @@ import * as Yup from 'yup';
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 import { Container, Content, Background } from './styles';
 import getValidationError from '../../utils/getValidationErrors';
@@ -20,9 +20,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn, user } = useContext(AuthContext);
-
-  console.log(user);
+  const { signIn  } = useAuth();
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
