d0d1113aefb321abaa9f1669704e7149dca00633
Mlodovico <murilo.lodovico@outlook.com>
2020-10-06 23:10:55 -0300

    1.10.84 implementing reset password page

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import { FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -13,6 +13,7 @@ import { useToast } from '../../hooks/toast';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
 import getValidationError from '../../utils/getValidationErrors';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -24,6 +25,9 @@ const ResetPassword: React.FC = () => {
   const { addToast } = useToast();
 
   const history = useHistory();
+  const location = useLocation();
+
+
   const handleSubmit = useCallback(async (data: ResetPasswordFormData) => {
     try {
       formRef.current?.setErrors({});
@@ -40,7 +44,20 @@ const ResetPassword: React.FC = () => {
         abortEarly: false,
       });
 
-      history.push('/signin');
+      const {password, password_confirmation} = data;
+      const token = location.search.replace('?token', '')
+
+      if(!token) {
+        throw new Error();
+      }
+
+      await api.post('password/reset', {
+        password,
+        password_confirmation,
+        token
+      })
+
+      history.push('/');
 
     } catch(err) {
       if(err instanceof Yup.ValidationError){
@@ -57,7 +74,7 @@ const ResetPassword: React.FC = () => {
       title: 'Erro ao resetar',
       description: 'Ocorreu um erro ao resetar sua senha, tente novamente',
     });
-  }, [addToast, history]);
+  }, [addToast, history, location.search]);
 
   return(
     <Container>
